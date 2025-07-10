04a9a999bd5eeff347880a154b3921cf78074b85
gywn1150 <gywn1150@gmail.com>
2025-07-10 20:03:41 +0900

    한효주 - Invader 프로젝트 소스 업로드

---
+++ Console.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "console.h"
+
+HANDLE hout;
+
+// 모든 화면 공백으로 클리어
+void ClearScreen()
+{
+	int i,j;
+	UPOINT pos;
+
+	for( i =1 ; i < 25 ; i++)
+	{
+		for( j = 1; j < 80 ; j++)
+		{
+			pos.x = j;
+			pos.y = i;
+			gotoxy(pos);
+			printf(" ");
+		}
+	}
+}
+
+// 화면 초기화
+void InitConsole()
+{
+	// 화면 커서 숨기기
+	CONSOLE_CURSOR_INFO coninfo;
+
+	coninfo.bVisible = FALSE;
+	coninfo.dwSize = 1;
+
+	hout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleCursorInfo(hout, &coninfo);
+}
+
+// 원하는 위치로 커서 강제 이동
+void gotoxy(UPOINT pt)
+{
+	COORD pos;
+
+	pos.X = pt.x;
+	pos.Y = pt.y;
+
+	SetConsoleCursorPosition(hout , pos);
+}
